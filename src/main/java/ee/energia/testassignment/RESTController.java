5ba94f26db1b870524cb89cc59bc0853c02aab30
sharifulbony <shariful.bony@gmail.com>
2019-10-25 22:57:56 +0300

    charge planner maven pushed to git

---
+++ src/main/java/ee/energia/testassignment/RESTController.java
@@ -0,0 +1,20 @@
+package ee.energia.testassignment;
+
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ *
+ */
+@RestController
+@RequestMapping(value = "")
+@Api(value = "Product Inventory System", description = "Operations related to Product Inventory Management System")
+
+public class RESTController {
+
+
+
+}
---
+++ src/main/webapp/WEB-INF/components/app.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="app">
+        <img src="./assets/logo.png">
+        <div class="ui container">
+            <my-vuetable></my-vuetable>
+        </div>
+    </div>
+</template>
+
+<script>
+    import ticketTable from '../components/ticketTable'
+
+    export default {
+        name: 'app',
+        components: {
+            ticketTable
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
